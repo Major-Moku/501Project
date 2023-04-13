c92fb725a1df875a74c6e1b92cae76caf2f709a4
songxu1216 <kimir7@icloud.com>
2023-04-13 15:54:06 -0400

    home demo added

--- Android/app/src/main/java/com/example/a501project/ui/home/HomeFragment.kt
+++ Android/app/src/main/java/com/example/a501project/ui/home/HomeFragment.kt
@@ -4,15 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.constraintlayout.widget.ConstraintSet
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.a501project.databinding.FragmentHomeBinding
-import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.a501project.R
+import androidx.recyclerview.widget.RecyclerView
+import com.example.a501project.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
@@ -22,103 +20,72 @@ class HomeFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-    private lateinit var gridLayout: ConstraintLayout
-    private lateinit var generateButton: Button
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val view: View = binding.root
-
-        gridLayout = binding.gridLayout
-        generateButton = binding.buttonGenerate
-
-        generateButton.setOnClickListener {
-            generateGrid(4, 4) // Replace the numbers with the desired number of rows and columns.
-        }
-
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
-        return view
+    ): View? {
+        return inflater.inflate(R.layout.fragment_game_list, container, false)
     }
 
-    private fun generateGrid(rows: Int, columns: Int) {
-        gridLayout.removeAllViews()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        val constraintSet = ConstraintSet()
-        var lastId: Int = View.NO_ID
+        // Create a list of games with their names and server status
+        val gameList = listOf(
+            Game("Apex Legends", R.drawable.game_a, true),
+            Game("DOTA2", R.drawable.game_b, false),
+            //Game("Game C", R.drawable.game_c, true),
+            //Game("Game D", R.drawable.game_d, true),
+            //Game("Game E", R.drawable.game_e, false)
+        )
+        val layoutManager = LinearLayoutManager(requireContext())
 
-        for (i in 0 until rows) {
-            var lastRowId: Int = View.NO_ID
+        // Find the RecyclerView in your layout
+        val recyclerView: RecyclerView = view.findViewById(R.id.recycler_view)
 
-            for (j in 0 until columns) {
-                val view = View(requireContext()).apply {
-                    id = View.generateViewId()
-                    setBackgroundColor(
-                        resources.getColor(
-                            android.R.color.holo_blue_light,
-                            requireContext().theme
-                        )
-                    )
-                    layoutParams = ConstraintLayout.LayoutParams(0, 0)
-                }
-
-                gridLayout.addView(view)
-                constraintSet.clone(gridLayout)
-
-                val topMargin = if (i == 0) 8 else 16
-                val startMargin = if (j == 0) 8 else 16
-
-                if (lastRowId != View.NO_ID) {
-                    constraintSet.connect(
-                        view.id,
-                        ConstraintSet.START,
-                        lastRowId,
-                        ConstraintSet.END,
-                        startMargin
-                    )
-                } else {
-                    constraintSet.connect(
-                        view.id,
-                        ConstraintSet.START,
-                        ConstraintSet.PARENT_ID,
-                        ConstraintSet.START,
-                        startMargin
-                    )
-                }
-                if (lastId != View.NO_ID) {
-                    constraintSet.connect(view.id, ConstraintSet.TOP, lastId, ConstraintSet.BOTTOM, topMargin)
-                } else {
-                    constraintSet.connect(view.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, topMargin)
-                }
-                constraintSet.constrainWidth(view.id, 0)
-                constraintSet.constrainHeight(view.id, 0)
-                constraintSet.constrainedWidth(view.id, true)
-                constraintSet.constrainedHeight(view.id, true)
-                constraintSet.setHorizontalWeight(view.id, 1f)
-                constraintSet.setVerticalWeight(view.id, 1f)
-
-                constraintSet.applyTo(gridLayout)
-
-                lastRowId = view.id
-            }
-            lastId = lastRowId
-        }
+        // Create a vertical list of cards using RecyclerView
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = GameAdapter(gameList)
     }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+}
+data class Game(val name: String, val imageRes: Int, val isOnline: Boolean)
+
+// Define a RecyclerView adapter for the list of games
+class GameAdapter(private val gameList: List<Game>) :
+    RecyclerView.Adapter<GameAdapter.ViewHolder>() {
+
+    // Inflate the layout for each item in the list
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_game, parent, false)
+        return ViewHolder(view)
+    }
+
+    // Set the data for each item in the list
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val game = gameList[position]
+        holder.gameName.text = game.name
+        holder.gameImage.setImageResource(game.imageRes)
+        if (game.isOnline) {
+            holder.statusIcon.setImageResource(R.drawable.ic_online)
+        } else {
+            holder.statusIcon.setImageResource(R.drawable.ic_offline)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return gameList.size
+    }
+
+    // Define a ViewHolder to hold the views for each item in the list
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val gameName: TextView = view.findViewById(R.id.game_name)
+        val gameImage: ImageView = view.findViewById(R.id.game_image)
+        val statusIcon: ImageView = view.findViewById(R.id.status_icon)
     }
 }
