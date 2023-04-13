b8742b80850b3fa8ef11103d6cf7cfca95b31eb8
ricardo <1127676571@qq.com>
2023-04-13 16:10:41 -0400

    opti(): item size

--- Android/app/src/main/java/com/example/a501project/ui/favorite/FavoriteFragment.kt
+++ Android/app/src/main/java/com/example/a501project/ui/favorite/FavoriteFragment.kt
@@ -5,10 +5,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
